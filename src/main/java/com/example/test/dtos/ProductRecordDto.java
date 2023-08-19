397383cc57f52e370a1cddb5a5ca118ce31500d7
Sensui <sensuijunior@dhotmail.com>
2023-08-19 14:48:38 -0300

    Post controller created

---
+++ src/main/java/com/example/test/dtos/ProductRecordDto.java
@@ -0,0 +1,14 @@
+package com.example.test.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(
+        @NotBlank
+        String name,
+        @NotNull
+        BigDecimal value
+) {
+}
